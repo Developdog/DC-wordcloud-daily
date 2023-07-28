b5e96049132f7a5d0abda4476db66f1bbf90214b
Developdog <youtaek123@naver.com>
2023-07-29 01:57:36 +0900

    변수 구분 및 워드클라우드 제작 방식 변경 및 직접 눌러서 쓰는 방식으로 변경

--- launcher.py
+++ launcher.py
@@ -2,19 +2,16 @@ import time, os
 from datetime import datetime
 
 # 대기 시간 : 초 기준
-waittime = 60
+waittime = 120
 
-#날짜 파일이 없을 경우 새로 생성
+# 날짜 파일이 없을 경우 새로 생성
 if not os.path.isfile('lastupd.txt'): open('lastupd.txt', 'w').write(datetime.now().strftime('%Y-%m-%d'))
 
 while (True):
-    #날짜 갱신이 안되어있을시 본격적 작업 시작
-    if datetime.strptime(open('lastupd.txt', 'r').read(), '%Y-%m-%d') < datetime.now().replace(hour=0, minute=0, second=0, microsecond=0):
+    # 날짜 파일 내부의 값이 오늘 날짜값과 다르다면 게시글 작업 시작
+    if datetime.strptime(open('lastupd.txt', 'r').read(), '%Y-%m-%d').date() < datetime.now().date():
 
-        #날짜 갱신
-        open('lastupd.txt', 'w').write(datetime.now().strftime('%Y-%m-%d'))
-
-        #스크립트 실행
+        # 스크립트 실행
         #os.system("lxterminal --command=\"python3 run.py\"")
         #Windows:
         #os.system("python3 run.py")
@@ -24,7 +21,9 @@ while (True):
             exec(code)
         # open을 이용해 해당 파이썬 파일을 열고, read를 이용해 해당 문서를 읽는다.
         # 이후 exec를 사용해 해당 코드를 읽는다.
-        # 파이썬 활용
+
+        open('lastupd.txt', 'w').write(str(datetime.now().date()))
+        # 모든 작업이 끝나면 날짜 파일의 날짜를 오늘로 갱신한다.
 
     print('[' + time.strftime('%Y-%m-%d %H:%M:%S') + ']' + '대기중... (' + str(waittime) + ')')
     time.sleep(waittime)
